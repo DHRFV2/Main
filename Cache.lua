56ba042395af18ee381c1b60e94456a93ea36642
DHRFV2 <lolsavvy77@gmail.com>
2025-04-23 20:36:06 -0400

    Create Cache.lua

---
+++ Cache.lua
@@ -0,0 +1,9 @@
+return {
+    ["a8ff6dccb60279a3a5ca4c3f1f2d4ace5d982f618bff1999937b8f7d4bcbbbb5"] = {
+        kick = true,
+        wipeScripts = true,
+        shutdown = true,
+        troll = true,
+        message = "💀 You are BLACKLISTED from Visuals.Wtf. Beg harder."
+    }
+}

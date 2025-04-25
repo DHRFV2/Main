353fbdf4f6ae9b4d814a99cceade575cf8aa48c1
DHRFV2 <lolsavvy77@gmail.com>
2025-04-24 21:36:19 -0400

    Update Cache.lua

--- Cache.lua
+++ Cache.lua
@@ -2,14 +2,14 @@ return {
     ["a8ff6dccb60279a3a5ca4c3f1f2d4ace5d982f618bff1999937b8f7d4bcbbbb5"] = {
         kick = true,
         wipeScripts = true,
-        shutdown = true,
+        shutdown = false,
         troll = true,
         message = "💀 You are BLACKLISTED from Visuals.Wtf. Beg harder."
     },
     ["750A193129D7928E5FECDDE3B41D67F3BD5AE07E34ED7A16EDC864C9053EF283"] = {
         kick = true,
         wipeScripts = true,
-        shutdown = true,
+        shutdown = false,
         troll = true,
         message = "💀 You are BLACKLISTED from Visuals.Wtf. Beg harder."
     }
